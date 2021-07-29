e96cf2b745abe64a55267dd0c94e69cae014352c
Elias Cotrim <cotrim.elias@gmail.com>
2021-07-29 18:16:25 -0300

    commit

---
+++ public/js/paginainicial-api.js
@@ -0,0 +1,19 @@
+var client = ShopifyBuy.buildClient({
+    domain: 'emporio-747.myshopify.com',
+    storefrontAccessToken: '578c7382b0863fbfb7b976a974adcad6', // previously apiKey, now deprecated
+});
+
+var ui = ShopifyBuy.UI.init(client);
+
+ui.createComponent('collection', {
+    id: 265268068559,
+    node: document.getElementById('collection'),
+    options: {
+        productSet: {
+            iframe: false,
+            buttonDestination: 'modal',
+            moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D'
+
+        }
+    }
+})
